68ebcde7814a5da6170c517454f25eccb28f38b8
Myclass <azatyuldashev1954gmail.com>
2019-11-20 19:57:48 +0500

    Add images model

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth import views
 from .views import index, other_page, SPasswordChangeView, LogoutView, SLoginView, RegisterFormView, news_list, news_detail, lessons_shedule
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 app_name = 'main'
 urlpatterns =[
     path('news/', news_list, name='news'),
@@ -14,3 +16,4 @@ urlpatterns =[
     path('accounts/login/', SLoginView.as_view(), name='login'),
     path('', index, name='index'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
